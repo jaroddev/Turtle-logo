dec32735fef43ef2d4c36460d51aa696f03217ed
Adnyropast <dealamu134579@gmail.com>
2019-12-12 15:04:29 +0100

    Update Token.java
    Tokens. They have a specific class and a value.
    Lexers will build lists of tokens for parsers to analyze.

--- src/ex/Token.java
+++ src/ex/Token.java
@@ -1,5 +1,29 @@
 package ex;
 
 public class Token {
-
+	private TokenClass tk;
+	private String value;
+	
+	public Token(TokenClass tk, String value) {
+		this.tk = tk;
+		this.value = value;
+	}
+	
+	public TokenClass getTokenClass() {
+		return tk;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public String toString() {
+		String string = tk.toString();
+		
+		if(value != null) {
+			string += "(\"" + value + "\")";
+		}
+		
+		return string;
+	}
 }
