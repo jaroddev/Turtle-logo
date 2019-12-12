b7cf41869f19dd2991bfce4717b2d3fc254fda6c
Adnyropast <dealamu134579@gmail.com>
2019-12-12 14:44:27 +0100

    getSymbolIndex method
    Created a method to get the column index in the transition table for a given symbol.

--- src/ex/Lexer.java
+++ src/ex/Lexer.java
@@ -1,5 +1,37 @@
 package ex;
 
-public class Lexer {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Lexer {
+	private int startState = 0;
+	private static Integer[][] transitions = {
+		{0,   1,   2,   102, 103, null},
+		{100, 1,   100, 100, 100, null},
+		{101, 101, 2,   101, 101, null}
+	};
+	
+	public int getSymbolIndex(Character symbol) {
+		if(symbol == null) {
+			return 0;
+		}
+		
+		else if(Character.isWhitespace(symbol)) {
+			return 0;
+		}
+		
+		else if(Character.isLetter(symbol)) {
+			return 1;
+		}
+		
+		else if(Character.isDigit(symbol)) {
+			return 2;
+		}
+		
+		else switch(symbol) {
+			case '[': return 3;
+			case ']': return 4;
+			default: return 5;
+		}
+	}
 }
