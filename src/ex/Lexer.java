40997a60baf6d0378965abf674697d581bebb571
Adnyropast <dealamu134579@gmail.com>
2019-12-21 17:05:38 +0100

    Completed Lexer class

--- src/ex/Lexer.java
+++ src/ex/Lexer.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 public class Lexer {
 	private int startState = 0;
 	private static Integer[][] transitions = {
-		{0,   1,   2,   102, 103, null},
-		{100, 1,   100, 100, 100, null},
-		{101, 101, 2,   101, 101, null}
+		{0,   1,   2,   100, 101, null},
+		{200, 1,   200, 200, 200, null},
+		{201, 201, 2,   201, 201, null}
 	};
 	
-	public int getSymbolIndex(Character symbol) {
+	private int getSymbolIndex(Character symbol) {
 		if(symbol == null) {
 			return 0;
 		}
@@ -33,5 +33,74 @@ public class Lexer {
 			case ']': return 4;
 			default: return 5;
 		}
+	}
+
+	public List<Token> lexer(String input) {
+		if(input == null) {return null;}
+		
+		input += " ";
+		
+		SourceReader sourceReader = new SourceReader(input);
+		
+		int state = startState;
+		
+		List<Token> tokens = new ArrayList<Token>();
+		StringBuffer buf = new StringBuffer();
+		
+		Character c;
+		
+		while((c = sourceReader.readSymbol()) != null) {
+			Integer nextState = transitions[state][getSymbolIndex(c)];
+			
+			// Error: transition not specified.
+			
+			if(nextState == null) {
+				return null;
+			}
+			
+			state = (int)nextState;
+			
+			if(state > 0) {
+				buf.append(c);
+			}
+			
+			// Accept states
+			
+			if(state >= 100) {
+				TokenClass tokenClass = null;
+				
+				switch(state) {
+					case 100: tokenClass = TokenClass.leftBracket; break;
+					case 101: tokenClass = TokenClass.rightBracket; break;
+					case 200: tokenClass = TokenClass.ident; break;
+					case 201: tokenClass = TokenClass.intVal; break;
+				}
+				
+				// "Go-back" states
+				
+				if(state >= 200) {
+					sourceReader.goBack();
+					buf.deleteCharAt(buf.length() - 1);
+				}
+				
+				// Creating the token.
+				
+				Token token;
+				
+				if(tokenClass != null) {
+					token = new Token(tokenClass, buf.toString());
+					
+					tokens.add(token);
+				}
+				
+				buf = new StringBuffer();
+				
+				// Returns to the startState.
+				
+				state = startState;
+			}
+		}
+		
+		return tokens;
 	}
 }
