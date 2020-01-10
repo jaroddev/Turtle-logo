2094e8be7721a44456975f9e20962d1b68d7b65b
Adnyropast <dealamu134579@gmail.com>
2020-01-10 12:06:29 +0100

    Update Parser.java comments
    Added comments explaining the grammar of the syntactic analysis.
    
    Also made the methods private.

--- src/ex/Parser.java
+++ src/ex/Parser.java
@@ -26,8 +26,25 @@ public class Parser {
 		return null;
 	}
 	
-	public NTree S() throws Exception {
-		if(getTokenClass() == TokenClass.ident) {
+	// S → ε | identAS
+	// A → intValB
+	// B → ε | [S]
+	
+	// First(S) = {ε, ident}
+	// First(A) = {intVal}
+	// First(B) = {ε, [}
+	
+	// Follow(S) = {EOF, ]}
+	// Follow(A) = First(S) ∪ Follow(S) = {EOF, ], ident}
+	// Follow(B) = Follow(A) = {EOF, ], ident}
+	
+	//      ident           intVal          [           ]       EOF
+	// S    S → identAS                                 S → ε   S → ε
+	// A                    A → intValB
+	// B    B → ε                           B → [S]     B → ε   B → ε
+	
+	private NTree S() throws Exception {
+		if(getTokenClass() == TokenClass.ident) {// S → identAS
 			Token token = getToken();
 			NTree tree = new NTree(token);
 			
@@ -46,19 +63,19 @@ public class Parser {
 			return tree;
 		}
 		
-		else if(getTokenClass() == TokenClass.rightBracket) {
+		else if(getTokenClass() == TokenClass.rightBracket) {// S → ε
 			return null;
 		}
 		
-		else if(isEOF()) {
+		else if(isEOF()) {// S → ε
 			return null;
 		}
 		
 		throw new Exception("Syntax Error: Unexpected token " + getToken().getValue());
 	}
 	
-	public NTree A() throws Exception {
-		if(getTokenClass() == TokenClass.intVal) {
+	private NTree A() throws Exception {
+		if(getTokenClass() == TokenClass.intVal) {// A → intValB
 			Token token = getToken();
 			NTree tree = new NTree(token);
 			
@@ -78,12 +95,12 @@ public class Parser {
 		throw new Exception("Syntax Error: Unexpected token " + getToken().getValue() + ", expected int value");
 	}
 	
-	public NTree B() throws Exception {
-		if(getTokenClass() == TokenClass.ident) {
+	private NTree B() throws Exception {
+		if(getTokenClass() == TokenClass.ident) {// B → ε
 			return null;
 		}
 		
-		else if(getTokenClass() == TokenClass.leftBracket) {
+		else if(getTokenClass() == TokenClass.leftBracket) {// B → [S]
 			Token token = getToken();
 			NTree tree = new NTree(token);
 			
@@ -111,11 +128,11 @@ public class Parser {
 			}
 		}
 		
-		else if(getTokenClass() == TokenClass.rightBracket) {
+		else if(getTokenClass() == TokenClass.rightBracket) {// B → ε
 			return null;
 		}
 		
-		else if(isEOF()) {
+		else if(isEOF()) {// B → ε
 			return null;
 		}
 		
